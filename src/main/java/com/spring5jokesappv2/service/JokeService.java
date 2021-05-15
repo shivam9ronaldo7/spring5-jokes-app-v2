38e668f1d0b09569e50df1f709423c7d0366d150
shivam9ronaldo7 <shivam9ronaldo7@gmail.com>
2021-05-15 20:19:32 +0530

    Joke application done

---
+++ src/main/java/com/spring5jokesappv2/service/JokeService.java
@@ -0,0 +1,5 @@
+package com.spring5jokesappv2.service;
+
+public interface JokeService {
+    String getJoke();
+}
---
+++ src/test/java/com/spring5jokesappv2/Spring5JokesAppV2ApplicationTests.java
@@ -0,0 +1,11 @@
+package com.spring5jokesappv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class Spring5JokesAppV2ApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
